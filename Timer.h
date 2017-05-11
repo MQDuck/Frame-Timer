3f78cad49bcf687d2fc75e0559acee0fb298153a
Jeffrey Thomas Piercy <mqduck@mqduck.net>
2017-05-11 13:13:12 -0700

    Change frames from deque to array. Keep running total of frames elements for use by getFrameTimeAverage() and getFPSAverage().

--- Timer.h
+++ Timer.h
@@ -1,36 +1,130 @@
 #include <chrono>
-#include <deque>
+//#include <deque>
 
-//namespace frametimer
-//{
+namespace frametimer
+{
 
 template<typename T = float>
 class Timer
 {
 private:
-    //int numFrames;
-    typename std::result_of<decltype(&std::deque<T>::size)(std::deque<T>)>::type numFrames;
-    std::deque<T> frames;
-    std::chrono::steady_clock::time_point time;
+    typedef std::chrono::steady_clock steady_clock;
+
+    T* frames;
+    int numFrames, numFramesActual, framesIndex;
+    steady_clock::time_point time;
+    T frameTimeTotal;
 
 public:
     Timer(const int numFrames = 1);
+    ~Timer() { delete[] frames; }
     bool setNumFrames(const int numFrames);
     const int getNumFrames() const { return numFrames; }
-    bool setTime(const std::chrono::steady_clock::time_point time);
-    const std::chrono::steady_clock::time_point getTime() const { return time; }
+    bool setTime(const steady_clock::time_point& time = steady_clock::now());
+    const steady_clock::time_point getTime() const { return time; }
     void ping();
-    T getFrameTime();
-    T getFrameTimeAverage();
-    T getFPS();
-    T getFPSAverage();
+    const T getFrameTime() const { return frames[framesIndex]; }
+    const T getFrameTimeAverage() const;
+    const T getFPS() const;
+    const T getFPSAverage() const;
 };
 
 template<typename T>
 Timer<T>::Timer(const int numFrames)
 {
-    time = std::chrono::steady_clock::now();
+    this->numFrames = 1;
     setNumFrames(numFrames);
+    time = steady_clock::now();
+}
+
+template<typename T>
+bool Timer<T>::setNumFrames(const int numFrames)
+{
+    if(numFrames < 1)
+        return false;
+
+    this->numFrames = numFrames;
+    if(frames)
+        delete[] frames;
+    frames = new T[numFrames]();
+    framesIndex = 0;
+    frameTimeTotal = 0;
+    numFramesActual = 0;
+    return true;
+}
+
+template<typename T>
+bool Timer<T>::setTime(const steady_clock::time_point& time)
+{
+    this->time = time;
+    return true;
+}
+
+template<typename T>
+void Timer<T>::ping()
+{
+    steady_clock::time_point timeOld = time;
+    time = steady_clock::now();
+    T frameTime = std::chrono::duration_cast<std::chrono::duration<T>>(time - timeOld).count();
+    frameTimeTotal -= frames[framesIndex];
+    frameTimeTotal += frameTime;
+    frames[framesIndex] = frameTime;
+    framesIndex = (framesIndex + 1) % numFrames;
+    if(numFramesActual < numFrames)
+        ++numFramesActual;
+}
+
+template<typename T>
+const T Timer<T>::getFrameTimeAverage() const
+{
+    if(numFramesActual == 0)
+        return 0;
+    return frameTimeTotal / numFramesActual;
+}
+
+template<typename T>
+const T Timer<T>::getFPS() const
+{
+    return 1 / getFrameTime();
+}
+
+template<typename T>
+const T Timer<T>::getFPSAverage() const
+{
+    return numFramesActual / frameTimeTotal;
+}
+
+/*template<typename T = float>
+class Timer
+{
+private:
+    typedef std::chrono::steady_clock steady_clock;
+
+    typename std::result_of<decltype(&std::deque<T>::size)(std::deque<T>)>::type numFrames;
+    std::deque<T> frames;
+    steady_clock::time_point time;
+    T frameTimeTotal;
+
+public:
+    Timer(const int numFrames = 1);
+    bool setNumFrames(const int numFrames);
+    const int getNumFrames() const { return numFrames; }
+    bool setTime(const steady_clock::time_point& time = steady_clock::now());
+    const steady_clock::time_point getTime() const { return time; }
+    void ping();
+    const T getFrameTime() const { return frames.front(); }
+    const T getFrameTimeAverage() const;
+    const T getFPS() const;
+    const T getFPSAverage() const;
+};
+
+template<typename T>
+Timer<T>::Timer(const int numFrames)
+{
+    this->numFrames = 1;
+    setNumFrames(numFrames);
+    frameTimeTotal = 0;
+    time = steady_clock::now();
 }
 
 template<typename T>
@@ -46,43 +140,45 @@ bool Timer<T>::setNumFrames(const int numFrames)
 }
 
 template<typename T>
+bool Timer<T>::setTime(const steady_clock::time_point& time)
+{
+    this->time = time;
+    return true;
+}
+
+template<typename T>
 void Timer<T>::ping()
 {
-    auto timeOld = time;
-    time = std::chrono::steady_clock::now();
-    frames.push_front(std::chrono::duration_cast<std::chrono::duration<T>>(time - timeOld).count());
+    steady_clock::time_point timeOld = time;
+    time = steady_clock::now();
+    T frameTime = std::chrono::duration_cast<std::chrono::duration<T>>(time - timeOld).count();
+    frames.push_front(frameTime);
+    frameTimeTotal += frameTime;
     if(frames.size() > numFrames)
+    {
+        frameTimeTotal -= frames.back();
         frames.pop_back();
+    }
 }
 
 template<typename T>
-T Timer<T>::getFrameTime()
-{
-    return frames.front();
-}
-
-template<typename T>
-T Timer<T>::getFrameTimeAverage()
+const T Timer<T>::getFrameTimeAverage() const
 {
     if(frames.empty())
         return 0;
-
-    T total = 0;
-    for(T time : frames)
-        total += time;
-    return total / frames.size();
+    return frameTimeTotal / frames.size();
 }
 
 template<typename T>
-T Timer<T>::getFPS()
+const T Timer<T>::getFPS() const
 {
     return 1 / getFrameTime();
 }
 
 template<typename T>
-T Timer<T>::getFPSAverage()
+const T Timer<T>::getFPSAverage() const
 {
-    return 1 / getFrameTimeAverage();
-}
+    return frames.size() / frameTimeTotal;
+}*/
 
-//} // namespace frametimer
+} // namespace frametimer
